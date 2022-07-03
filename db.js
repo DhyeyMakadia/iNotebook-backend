c4c2569781a80fafda1fd1654f005a38f507eff4
DMak <dj.makadia24@gmail.com>
2022-07-03 11:37:47 +0530

    Initial Commit

---
+++ db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+const mongoURI = 'mongodb://127.0.0.1:27017'
+
+const connectToMongo = () => {
+    mongoose.connect(mongoURI,()=>{
+        console.log('Connected to Database.');
+    })
+}
+
+
+module.exports = connectToMongo;
