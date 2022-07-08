ac4b94a750af0747a1d180d730813329cb522d95
DMak <dj.makadia24@gmail.com>
2022-07-08 17:48:47 +0530

    Changed port and moved to secure file

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const connectToMongo = require('./db')
 const express = require('express')
+require("dotenv").config();
 connectToMongo();
 
 const app = express()
-const port = 3000
+const PORT = process.env.PORT
 app.use(express.json())
 
 // Available Routes
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
 })
